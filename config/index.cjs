8e3b9cd3c959efff7fb4156d61fa237c001c05c7
CcSyyy <113076834+CcSyyy@users.noreply.github.com>
2024-02-23 17:06:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -46,7 +46,7 @@ const USER_CONFIG = {
     
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'YU2WbwsUpj2hFcQmX8k_8zNFtcDt1UjE8xpUB3ftKRQ',
+  //  CALLBACK_TEMPLATE_ID: 'YU2WbwsUpj2hFcQmX8k_8zNFtcDt1UjE8xpUB3ftKRQ',
 
   CALLBACK_USERS: [
     {
